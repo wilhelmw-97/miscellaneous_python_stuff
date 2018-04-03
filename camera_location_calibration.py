3f1ec22a76b3de38468497e3780bd045169f0043
Wilhelm <wbw201201@163.com>
2018-04-04 02:12:51 +0800

    finishing node 1

--- camera_location_calibration.py
+++ camera_location_calibration.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python
 import rospy, tf, tf2_ros, geometry_msgs, sys
 
+'''
+usage:
+for calibration of coordinates of markers
+and for coordination of 2 cameras
 
-parent_frame_name = sys.argv[1]
-parent_trans_x = sys.argv[2]
-parent_trans_y = sys.argv[3]
-parent_trans_z = sys.argv[4]
-parent_rot_roll = sys.argv[5]
-parent_rot_pitch = sys.argv[6]
-parent_rot_yaw = sys.argv[7]
-child_frame_name = sys.argv[8]
-child_trans_x = sys.argv[9]
-child_trans_y = sys.argv[10]
-child_trans_z = sys.argv[11]
-child_rot_roll = sys.argv[12]
-child_rot_pitch = sys.argv[13]
-child_rot_yaw = sys.argv[14]
+1. 2 cameras should report same location for the same marker
+2. get the coordinates of waypoints with this node
 
-if len(sys.argv) != 15:
-    print 'incorrect number of args.'
+
+'''
+
+rospy.init_node('tf2_turtle_broadcaster')
+print 'publishing transformations. using this node in a launch file is suggested.'
+if len(sys.argv) != 8:
+    print 'incorrect number of args. give 8 args:'
+    print 'parent frame, child frame, x, y, z, roll, pitch, yaw'
+
 
     exit(1)
 
 br = tf2_ros.TransformBroadcaster()
 
-
-
-
-def handle_turtle_pose(msg, turtlename):
-
+parent_frame_name = sys.argv[1]
+child_frame_name = sys.argv[2]
+trans_x = sys.argv[3]
+trans_y = sys.argv[4]
+trans_z = sys.argv[5]
+roll = sys.argv[6]
+pitch = sys.argv[7]
+yaw = sys.argv[8]
+rate = rospy.Rate(201)
+while not rospy.is_shutdown():
+    rate.sleep()
     t = geometry_msgs.msg.TransformStamped()
 
     t.header.stamp = rospy.Time.now()
-    t.header.frame_id = "world"
-    t.child_frame_id = turtlename
-    t.transform.translation.x = msg.x
-    t.transform.translation.y = msg.y
-    t.transform.translation.z = 0.0
-    q = tf.transformations.quaternion_from_euler(0, 0, msg.theta)
+    t.header.frame_id = sys.argv[1]
+    t.child_frame_id = sys.argv[2]
+    t.transform.translation.x = sys.argv[3]
+    t.transform.translation.y = sys.argv[4]
+    t.transform.translation.z = sys.argv[5]
+    q = tf.transformations.quaternion_from_euler(sys.argv[6], sys.argv[7], sys.argv[8])
     t.transform.rotation.x = q[0]
     t.transform.rotation.y = q[1]
     t.transform.rotation.z = q[2]
@@ -45,11 +50,4 @@ def handle_turtle_pose(msg, turtlename):
 
     br.sendTransform(t)
 
-if __name__ == '__main__':
-    rospy.init_node('tf2_turtle_broadcaster')
-    turtlename = rospy.get_param('~turtle')
-    rospy.Subscriber('/%s/pose' % turtlename,
-                     turtlesim.msg.Pose,
-                     handle_turtle_pose,
-                     turtlename)
-    rospy.spin()
+
